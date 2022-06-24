a069d9ba00b6c8dcbe675606c23811f9720aced0
Spencer Berggreen <spencerberggreen@gmail.com>
2022-06-24 09:36:53 -0500

    solve 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
-};
+function reverseString(str) {
+    // split string into an array
+    let splitString = str.split('');
+    // reverse the array
+    let reverseArray = splitString.reverse('');
+    // concatenate the array
+    let joinArray = reverseArray.join('');
+    // return the reversed string
+    return joinArray;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
