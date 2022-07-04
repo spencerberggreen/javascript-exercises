d4334aa7ed0cfbec5ec9100e17ebc05b595a7cd9
Spencer Berggreen <spencerberggreen@gmail.com>
2022-07-04 09:35:03 -0500

    solve 02 and 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,13 @@
 function reverseString(str) {
     // split string into an array
     let splitString = str.split('');
+
     // reverse the array
     let reverseArray = splitString.reverse('');
+
     // concatenate the array
     let joinArray = reverseArray.join('');
+
     // return the reversed string
     return joinArray;
 }
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (...args) {
+    // pull first argument from the function (which is the array)
+    const array = args[0];
+    // create a new array
+    const newArray = [];
+    // use forEach to go through the array
+    array.forEach((item) => {
+        // push every element into the new array
+        // UNLESS it is included in the function arguments
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    // and return that array
+    return newArray;
 };
 
 // Do not edit below this line
